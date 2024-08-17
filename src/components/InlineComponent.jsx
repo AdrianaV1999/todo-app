54e2d8fb42b5f1b683fad22901659055dfbbb71c
Adriana <vadriana258@gmail.com>
2024-08-17 19:59:24 +0200

    first commit

---
+++ src/components/InlineComponent.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./inlinecomponent.module.css";
+function InlineComponent() {
+  return (
+    <div>
+      <h1 className={styles.header}>InlineComponent</h1>
+    </div>
+  );
+}
+
+export default InlineComponent;
---
+++ src/components/OutlineComponent.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./outlinecomponent.module.css";
+function OutlineComponent() {
+  return (
+    <div>
+      <h1 className={styles.header}>This is an outline component</h1>
+    </div>
+  );
+}
+
+export default OutlineComponent;
